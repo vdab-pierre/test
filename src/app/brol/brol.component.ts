b314cf8cc2161431f90529c7ba06286eb1657d5b
vdab-pierre <pierre.colin@vdab.be>
2023-12-07 13:41:14 +0100

    oeps, nu met heroes ;)

--- src/app/brol/brol.component.ts
+++ src/app/brol/brol.component.ts
@@ -1,12 +1,23 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-brol',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,],
   templateUrl: './brol.component.html',
   styleUrl: './brol.component.css'
 })
 export class BrolComponent {
-
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
